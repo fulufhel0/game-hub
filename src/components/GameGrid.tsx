a000f39588ac3d1edd80d3c63df038ea8905a32d
Fulufhelo Mukwevho <fulufhelomukwevho@Fulufhelos-MacBook-Air.local>
2025-03-08 13:06:53 +0200

    build game card container

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "./hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
     const {games, error, isLoading} = useGames();
@@ -10,8 +11,8 @@ const GameGrid = () => {
     <>
     {error && <Text color="red.500" fontWeight="bold">{error}</Text>}
     <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing="4" padding="10px">
-        { isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-        { games.map(game => <GameCard key={game.id} game={game} />)}
+        { isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton/></GameCardContainer>)}
+        { games.map(game => <GameCardContainer key={game.id}><GameCard key={game.id} game={game} /></GameCardContainer>)}
     </SimpleGrid>
     </>
   )
