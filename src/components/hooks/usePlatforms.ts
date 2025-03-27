84b94ef48f61b00444789d9d483d5efc18eeb7b4
Fulu Mukwevho <f.mukwevho@icloud.com>
2025-03-27 20:36:53 +0200

    Refactoring platforms filter to use react query

--- src/components/hooks/usePlatforms.ts
+++ src/components/hooks/usePlatforms.ts
@@ -1,10 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../../data/PlatformsDataSource"
+import apiClient from "../../services/api-client";
+import { FetchResponse } from "./useData";
 export interface Platform{
     id: number;
     name: string;
     slug: string
 }
 
-const usePlatforms = () => ({data: platforms, isLoading:null, error:null})
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/list/parents').then((response) => response.data),
+    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    initialData: { count: 0, results: platforms }
+});
 
 export default usePlatforms;
